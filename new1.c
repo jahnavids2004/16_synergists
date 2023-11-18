1f69e84bfa4ccb9d3525776ab2da6d161d0029d9
jahnavids2004 <147744733+jahnavids2004@users.noreply.github.com>
2023-11-18 13:14:52 +0530

    initial commit

--- new1.c
+++ new1.c
@@ -1,40 +1,44 @@
-#include <opencv2/opencv.hpp>
-#include <dlib/image_processing.hpp>
-#include <dlib/facelandmark.hpp>
+#include<stdio.h>
+#include<stdlib.h>
+#include<dlib.h>
+#include<string.h>
+#include<cv.h>
 
-using namespace cv;
 using namespace dlib;
 
-// Function to load known faces and their encodings
+vector<IplImage*> loadsavedFaces() {
+  vector<IplImage*> savedFaces;
 
-vector<Mat> loadKnownFaces() {
-  vector<Mat> knownFaces;
-  
-  // Load face images and convert them to matrices
-  for (const string& facePath : knownFacePaths) {
-    Mat faceImage = imread(facePath);
-    knownFaces.push_back(faceImage);
+  for (const string& facePath : savedFacesPaths) {
+    IplImage* faceImage = cvLoadImage(facePath.c_str());
+    if (!faceImage) {
+      printf("Error loading known face image: %s\n", facePath.c_str());
+      exit(1);
+    }
+   savedFaces.push_back(faceImage);
   }
-  return knownFaces;
+  return savedFaces;
 }
 
-// Function to detect faces and extract encodings from an image
-vector<dlib::rectangle> detectFacesAndExtractEncodings(const Mat& image) {
-  // Convert image to grayscale
-  Mat grayImage;
-  cvtColor(image, grayImage, COLOR_BGR2GRAY);
+vector<dlib::rectangle> detectFacesAndExtractEncodings(IplImage* image) {
+ 
+  IplImage* grayImage = cvCreateImage(cvGetSize(image), image->depth, 1);
+  cvCvtColor(image, grayImage, CV_BGR2GRAY);
 
-  // Detect faces using dlib's face detection algorithm
+  
+
   frontal_face_detector detector;
-  std::vector<dlib::rectangle> faces = detector(grayImage);
+  std::vector<dlib::rectangle> faces = detector((cv_matrix)grayImage);
 
-  // Extract encodings for each detected face
+  
+
   std::vector<dlib::shape> shapes;
   shape_predictor predictor;
   deserialize(predictor, "shape_predictor_68_face_landmarks.dat");
-  extract_face_features(grayImage, faces, shapes);
+  extract_face_features((cv_matrix)grayImage, faces, shapes);
 
-  // Convert dlib shapes to OpenCV Mat format
+  
+
   vector<Mat> encodings;
   for (const dlib::shape& shape : shapes) {
     Mat encoding = Mat(1, 128, CV_32F);
@@ -44,31 +48,38 @@ vector<dlib::rectangle> detectFacesAndExtractEncodings(const Mat& image) {
     encodings.push_back(encoding);
   }
 
-  return encodings;
+
+  cvReleaseImage(&grayImage);
+
+  return encodings;
 }
 
-// Function to compare face encodings and mark attendance
-void compareFacesAndMarkAttendance(const vector<Mat>& knownEncodings, const vector<Mat>& currentEncodings) {
-  // Compare each current encoding with the known encodings
+void CFAMA(const vector<IplImage*>& savedFaces, const vector<Mat>& currentEncodings) {
+
+  
   for (int i = 0; i < currentEncodings.size(); i++) {
     Mat currentEncoding = currentEncodings[i];
-    for (int j = 0; j < knownEncodings.size(); j++) {
-      Mat knownEncoding = knownEncodings[j];
+    for (int j = 0; j < savedFaces.size(); j++) {
+      IplImage* savedFace = savedFaces[j];
+      Mat knownEncoding = extractFaceEncoding(savedFace);
 
-      // Calculate the distance between the encodings
-      double distance = compareFaceDescriptors(currentEncoding, knownEncoding, 0.5);
+   
+ 
 
-      // If the distance is small enough, mark attendance for the corresponding person
-      if (distance < 0.5) {
-        // Identify the corresponding person
-        string name = knownFaceNames[j];
+      double distance = compareFaceDescriptors(currentEncoding, knownEncoding, 0.8);
 
-        // Mark attendance for the person
+      
+      if (distance < 0.8) {
+        
+        string name = savedFaceNames[j];
+
+      
         markAttendance(name);
-      }
-    }
-  }
+      }
+    }
+  }
 }
+
 
 // Function to mark attendance for a person
 void markAttendance(const string& name) {
@@ -81,20 +92,37 @@ void markAttendance(const string& name) {
 
 int main() {
   // Load known faces and their encodings
-  vector<Mat> knownFaces = loadKnownFaces();
+  vector<IplImage*> savedFaces = loadsavedFaces();
 
-  // Capture an image or load an existing image
-  Mat image = imread("test.jpg"); // or captureImageFromWebcam()
+  // Initialize webcam capture
+  CvCapture* videoCapture = cvCaptureFromCAM(0);
 
-  // Detect faces and extract encodings
-  vector<Mat> currentEncodings = detectFacesAndExtractEncodings(image);
+  while (true) {
+    // Capture a frame from the webcam
+    IplImage* frame = cvQueryFrame(videoCapture);
+    if (!frame) {
+      break;
+    }
 
-  // Compare face encodings and mark attendance
-  compareFacesAndMarkAttendance(knownFaces, currentEncodings);
+// Display the frame
 
-  // Display the image with marked faces (if applicable)
+  cvShowImage("Webcam Feed", frame);
 
-  // Print attendance records
+  // Check ESC pressed or not so that the displat exists and code restarts
 
-  return 0;
-}
+  if (cvWaitKey(1) & 0xFF == 27) {
+    break;
+  }
+
+
+
+
+    // Detect the faces to extract the encodings
+
+    vector<Mat> currentEncodings = detectFacesAndExtractEncodings(frame);
+
+    // Compare face encodings and mark attendance
+    CFAMA(savedFaces, currentEncodings);
+
+    // Display the frame with marked faces
+    cv
